fd268893fc10434432d22318b44ef55b69b288d3
Aurora Gaffney <aurora@blinklabs.io>
2024-04-10 09:02:04 -0500

    feat: listeners support (#2)

---
+++ config.go
@@ -0,0 +1,109 @@
+// Copyright 2024 Blink Labs Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package node
+
+import (
+	"fmt"
+	"io"
+	"log/slog"
+
+	ouroboros "github.com/blinklabs-io/gouroboros"
+)
+
+type Config struct {
+	logger       *slog.Logger
+	listeners    []ListenerConfig
+	network      string
+	networkMagic uint32
+	peerSharing  bool
+	// TODO
+}
+
+// configPopulateNetworkMagic uses the named network (if specified) to determine the network magic value (if not specified)
+func (n *Node) configPopulateNetworkMagic() error {
+	if n.config.networkMagic == 0 && n.config.network != "" {
+		tmpCfg := n.config
+		tmpNetwork := ouroboros.NetworkByName(n.config.network)
+		if tmpNetwork == ouroboros.NetworkInvalid {
+			return fmt.Errorf("unknown network name: %s", n.config.network)
+		}
+		tmpCfg.networkMagic = tmpNetwork.NetworkMagic
+		n.config = tmpCfg
+	}
+	return nil
+}
+
+func (n *Node) configValidate() error {
+	if n.config.networkMagic == 0 {
+		return fmt.Errorf("invalid network magic value: %d", n.config.networkMagic)
+	}
+	if len(n.config.listeners) == 0 {
+		return fmt.Errorf("no listeners defined")
+	}
+	return nil
+}
+
+// ConfigOptionFunc is a type that represents functions that modify the Connection config
+type ConfigOptionFunc func(*Config)
+
+// NewConfig creates a new node config with the specified options
+func NewConfig(opts ...ConfigOptionFunc) Config {
+	c := Config{
+		// Default logger will throw away logs
+		// We do this so we don't have to add guards around every log operation
+		logger: slog.New(slog.NewJSONHandler(io.Discard, nil)),
+		// TODO: add defaults
+	}
+	// Apply options
+	for _, opt := range opts {
+		opt(&c)
+	}
+	return c
+}
+
+// WithLogger specifies the logger to use. This defaults to discarding log output
+func WithLogger(logger *slog.Logger) ConfigOptionFunc {
+	return func(c *Config) {
+		c.logger = logger
+	}
+}
+
+// WithListeners specifies the listener config(s) to use
+func WithListeners(listeners ...ListenerConfig) ConfigOptionFunc {
+	return func(c *Config) {
+		c.listeners = append(c.listeners, listeners...)
+	}
+}
+
+// WithNetwork specifies the named network to operate on. This will automatically set the appropriate network magic value
+func WithNetwork(network string) ConfigOptionFunc {
+	return func(c *Config) {
+		c.network = network
+	}
+}
+
+// WithNetworkMagic specifies the network magic value to use. This will override any named network specified
+func WithNetworkMagic(networkMagic uint32) ConfigOptionFunc {
+	return func(c *Config) {
+		c.networkMagic = networkMagic
+	}
+}
+
+// WithPeerSharing specifies whether to enable peer sharing. This is disabled by default
+func WithPeerSharing(peerSharing bool) ConfigOptionFunc {
+	return func(c *Config) {
+		c.peerSharing = peerSharing
+	}
+}
--- go.mod
+++ go.mod
@@ -2,9 +2,19 @@ module github.com/blinklabs-io/node
 
 go 1.21.5
 
-require github.com/spf13/cobra v1.8.0
+require (
+	github.com/blinklabs-io/gouroboros v0.78.0
+	github.com/spf13/cobra v1.8.0
+)
 
 require (
+	github.com/fxamacker/cbor/v2 v2.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jinzhu/copier v0.4.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/utxorpc/go-codegen v0.4.4 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
 )
